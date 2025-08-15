00a8d619f6cce02bba376813e7c9e0359b513572
Rithsagea <rithsagea@gmail.com>
2025-08-14 21:03:03 -0400

    ui fixes

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -13,7 +13,8 @@ return {
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
       },
       astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
